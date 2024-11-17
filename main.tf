bd3cd01209f9930e74aa3dcfbec4ced837a1d155
Abhinav Shrivastava <shrivastava.abhinav101@gmail.com>
2024-11-17 11:06:49 +0530

    added file by me1

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "foo" {
-  ami           = "ami-05fa00d4c63e32376" # us-west-2
+  ami           = "ami-0866a3c8686eaeeba" 
   instance_type = "t2.micro"
   tags = {
       Name = "TF-Instance"
